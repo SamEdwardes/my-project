d3ecb3feec9a5e7a987531465fb4de0b1e836013
SamEdwardes <edwardes.s@gmail.com>
2022-09-20 03:04:01 +0000

    setup renv

--- app.R
+++ app.R
@@ -1,43 +1,25 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
-library(palmerpenguins)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
     # Application title
     titlePanel("Old Faithful Geyser Data"),
-
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            sliderInput("bins", "Number of bins:", min = 1, max = 50, value = 30)
         ),
-
         # Show a plot of the generated distribution
         mainPanel(
            plotOutput("distPlot"),
            tableOutput("penguinsData"),
-           textOutput("libData")
+           verbatimTextOutput("libData")
         )
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
     output$distPlot <- renderPlot({
         # generate bins based on input$bins from ui.R
         x    <- faithful[, 2]
@@ -48,13 +30,8 @@ server <- function(input, output) {
              xlab = 'Waiting time to next eruption (in mins)',
              main = 'Histogram of waiting times')
     })
-    
-    output$penguinsData <- renderTable({
-      head(penguins)
-    })
-    
     output$libData <- renderText({
-      paste(.libPaths(), collapse = " ||| ")
+      .libPaths()
     })
 }
 
